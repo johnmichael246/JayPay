249919c0c9c878fdd44128dc94d811b3a26affa3
john solis <1johnmichael.solis@gmail.com>
2017-07-28 02:12:00 -0700

    fixed error in checks controller

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -1,29 +1,30 @@
 class ChecksController < ApplicationController
-end
-before_action :authorize
 
-def show
-    @check = Check.find(params[:id])
-end
+    before_action :authorize
+    def show
+        @check = Check.find(params[:id])
+    end
 
-def index
-    @prid = params[:payroll_id]
-    if (@prid)
-        Check.find({payroll_id:prid})
-    else
-        @checks = Check.where(@current_payroll)
+    def index
+        @prid = params[:payroll_id]
+        if (@prid)
+            Check.find({payroll_id:prid})
+        else
+            @checks = Check.where(@current_payroll)
+        end
+    end
+
+    def new
+        @check = Check.new(params.require(:check).permit(:check_total,:check_name))
+        @payroll = Payroll.create(end_date: @pay_period_date)
+        unless @checks
+            @checks = Check.create()
+        end 
+    end
+
+    def create
     end
 end
 
-def new
-    @check = Check.new(params.require(:check).permit(:check_total,:check_name))
-    @payroll = Payroll.create(end_date: @pay_period_date)
-    unless @checks
-        @checks = Check.create()
-    end 
-end
-
-def create
-end
 
 
